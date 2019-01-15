121c4aac876bce5aa3f1b31069df34f09d9b9056
白鹏 <243848737@qq.com>
2019-01-15 17:57:36 +0800

    添加统一配置中心

---
+++ server/src/main/java/com/imooc/order/server/controller/EnvController.java
@@ -0,0 +1,25 @@
+package com.imooc.order.server.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 查询统一配置中心的文件是否正确
+ * Created By 白鹏
+ * 2019/1/1516:45
+ */
+@RestController
+@RequestMapping("/env")
+public class EnvController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/print")
+    public String print(){
+
+        return env;
+    }
+}
