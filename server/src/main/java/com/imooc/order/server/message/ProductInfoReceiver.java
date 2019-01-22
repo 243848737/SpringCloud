57f16eafd6e4164011fd31d0bdbf45290f012288
白鹏 <243848737@qq.com>
2019-01-22 11:17:28 +0800

    接收商品消息

--- server/src/main/java/com/imooc/order/server/message/ProductInfoReceiver.java
+++ server/src/main/java/com/imooc/order/server/message/ProductInfoReceiver.java
@@ -1,13 +1,15 @@
 package com.imooc.order.server.message;
 
 import com.alibaba.fastjson.JSONObject;
-import com.imooc.order.server.utils.JsonUtil;
 import com.imooc.product.common.ProductInfoOutPut;
-import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 接收商品消息
@@ -18,10 +20,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductInfoReceiver {
 
+    private static  final String PRODUCT_STOCK_TEMPLATE = "product_stock_%s";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     private void process(String message){
         //ProductInfoOutPut outPut=(ProductInfoOutPut) JsonUtil.formJosn(message,ProductInfoOutPut.class);
-        ProductInfoOutPut outPut=JSONObject.parseObject(message,ProductInfoOutPut.class);
-        log.info("从【{}】接收到消息：{}","productInfo",outPut);
+        List<ProductInfoOutPut> productInfoOutPutList=JSONObject.parseArray(message,ProductInfoOutPut.class);
+        log.info("从【{}】接收到消息：{}","productInfo",productInfoOutPutList);
+
+        //将消息存入Redis
+        for (ProductInfoOutPut outPut:productInfoOutPutList) {
+            stringRedisTemplate.opsForValue().set(String.format(PRODUCT_STOCK_TEMPLATE, outPut.getProductId()), outPut.getProductStock().toString());
+        }
     }
 }
