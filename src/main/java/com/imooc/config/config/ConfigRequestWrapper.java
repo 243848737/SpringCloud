237f5eed8a362a6470b3dbfe362b61c47ffe99b2
白鹏 <243848737@qq.com>
2019-01-25 11:00:58 +0800

    添加Bus

--- src/main/java/com/imooc/config/config/ConfigRequestWrapper.java
+++ src/main/java/com/imooc/config/config/ConfigRequestWrapper.java
@@ -3,7 +3,6 @@ package com.imooc.config.config;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
